ff17cbde6c0e45aea3c5d1b2ce89fa56319fd7b7
Timo Haeckel <github@timohaeckel.de>
2021-10-01 15:07:00 +0200

    add weather skill

---
+++ src/wheatherskill.py
@@ -0,0 +1,75 @@
+"""
+Skill to fetch weather state
+
+WeatherState is a state data object for the WeatherSkill.
+WeatherSkill is a Skill to fetch current weather information.
+
+Copyright (c) 2021 Timo Haeckel
+"""
+
+from skills import SkillWithState
+import requests
+import json
+
+
+class WeatherSkill(SkillWithState):
+    """ Skill to detect devices present in the network.
+
+    This skill sends ping probes to the device addresses to detect their 
+    presence. Remember that a host may not respond to a ping (ICMP) request
+    even if the host name is valid.
+    The DevicePresenceState() with the key STATE_PREFIX+ADDRESS will be
+    constantly updated.
+
+    Attributes
+    ----------
+    addresses : list(str)
+        The device IPv4 addresses to be detected in the local network.
+    STATE_PREFIX : str
+        A prefix used to form the state key in the state data base with 
+        STATE_PREFIX + Address  
+    """
+    STATE_PREFIX = "Weather"
+
+    def __init__(self,
+                 statedb,
+                 weatherRequestURL,
+                 interval=60,
+                 errorSilent=False,
+                 logSilent=False,
+                 logFile=""):
+        """ 
+        Parameters
+        ----------
+        statedb : statedb.StateDataBase
+            The shared state data base instance used for all skills in 
+            the home automation setup
+        weatherRequestURL : str
+            URL to fetch the weather information from
+        interval : int (Default 60)   
+            The interval to fetch weather information in seconds
+        errorSilent : Boolean (Default False)
+            True if errors shall not be printed
+        logSilent : Boolean (Default False)
+            True if log messages shall not be printed
+        logFile : str (Default "")
+            Path to the log file to be used for errors and log messages
+        """
+        SkillWithState.__init__(self,
+                                name="DetectDevicePresence",
+                                statedb=statedb,
+                                interval=interval,
+                                errorSilent=errorSilent,
+                                logSilent=logSilent,
+                                logFile=logFile)
+        self.requestURL = weatherRequestURL
+
+    def task(self):
+        """ Refreshes the weather information
+
+        This function will refresh the weather information for a request url.
+        The weather json data with the key STATE_PREFIX will be updated.
+        """
+        weather = requests.get(self.requestURL).json()
+        self.log("current weather:\n" + json.dumps(weather, indent=4))
+        self.updateState(self.STATE_PREFIX, weather)
